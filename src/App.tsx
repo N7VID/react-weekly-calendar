8fb07255673f829a570895219a456893e2c8c8d7
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-09-21 13:43:49 +0330

    feat: Inter font

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import Calendar from "./components/Calendar/Calendar";
 
 function App() {
   return (
-    <div>
+    <div className="flex justify-center items-center flex-col min-h-screen gap-8 font-Inter cursor-default">
+      <h1 className="text-2xl font-bold">Weekly Calendar</h1>
       <Calendar />
     </div>
   );
