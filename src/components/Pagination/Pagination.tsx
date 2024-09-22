c44915709dddfcf46365c9ef3123f9e0eee484fc
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-09-22 21:38:27 +0330

    fix: pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,14 +1,25 @@
 import ChevronLeftIcon from "../../assets/svg/ChevronLeftIcon";
 import ChevronRightIcon from "../../assets/svg/ChevronRightIcon";
 
-export default function Pagination() {
+interface Iprops {
+  goToPreviousWeek: () => void;
+  goToNextWeek: () => void;
+}
+
+export default function Pagination({ goToPreviousWeek, goToNextWeek }: Iprops) {
   return (
     <div className="border-t-2 border-black pb-4 pt-3 px-4 text-center flex justify-between items-center">
-      <div className="flex justify-center items-center gap-2 cursor-pointer">
+      <div
+        className="flex justify-center items-center gap-2 cursor-pointer"
+        onClick={goToPreviousWeek}
+      >
         <ChevronLeftIcon />
         هفته قبلی
       </div>
-      <div className="flex justify-center items-center gap-2 cursor-pointer">
+      <div
+        className="flex justify-center items-center gap-2 cursor-pointer"
+        onClick={goToNextWeek}
+      >
         هفته بعدی
         <ChevronRightIcon />
       </div>
