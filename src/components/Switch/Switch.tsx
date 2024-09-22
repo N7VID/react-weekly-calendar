38f282b03ea4b51d00769961237e570574018393
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-09-22 23:20:49 +0330

    fix: style improvement Switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -6,15 +6,19 @@ interface Iprops {
 export default function Switch({ onClick, language }: Iprops) {
   return (
     <div className="flex justify-end items-center -mb-14">
-      <div className="w-16">
+      <div className="w-20">
         <div
-          className="relative bg-black max-w-[45px] min-h-[25px] mx-auto rounded-3xl flex items-center cursor-pointer"
+          className={`relative max-w-[45px] min-h-[25px] mx-auto rounded-3xl flex items-center cursor-pointer transition-colors duration-300
+        ${
+          language === "jalali"
+            ? "bg-blue-500 hover:bg-blue-400"
+            : "bg-gray-600 hover:bg-gray-500"
+        }`}
           onClick={onClick}
         >
           <div
-            className={`bg-gray-200 w-5 h-5 absolute top-[2.5px] right-[2.5px] rounded-full transition duration-300 ${
-              language === "jalali" ? "-translate-x-[21.5px]" : ""
-            }`}
+            className={`bg-white w-5 h-5 absolute top-[2.5px] right-[2.5px] rounded-full transition-transform duration-300
+          ${language === "jalali" ? "-translate-x-[21.5px]" : ""}`}
           ></div>
         </div>
       </div>
