94424b9d357de0bd416d12963f125cc478103ac6
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-09-22 23:25:23 +0330

    fix: Language of pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -4,9 +4,14 @@ import ChevronRightIcon from "../../assets/svg/ChevronRightIcon";
 interface Iprops {
   goToPreviousWeek: () => void;
   goToNextWeek: () => void;
+  language: "jalali" | "gregorian";
 }
 
-export default function Pagination({ goToPreviousWeek, goToNextWeek }: Iprops) {
+export default function Pagination({
+  goToPreviousWeek,
+  goToNextWeek,
+  language,
+}: Iprops) {
   return (
     <div className="border-t-2 border-gray-300 pb-4 pt-3 px-4 text-center flex justify-between items-center">
       <div
@@ -14,13 +19,14 @@ export default function Pagination({ goToPreviousWeek, goToNextWeek }: Iprops) {
         onClick={goToPreviousWeek}
       >
         <ChevronLeftIcon />
-        هفته قبلی
+        {language === "jalali" ? "هفته قبلی" : "Previous Week"}
       </div>
       <div
         className="flex justify-center items-center gap-2 cursor-pointer"
         onClick={goToNextWeek}
       >
-        هفته بعدی
+        {language === "jalali" ? "هفته بعدی" : "Next Week"}
+
         <ChevronRightIcon />
       </div>
     </div>
